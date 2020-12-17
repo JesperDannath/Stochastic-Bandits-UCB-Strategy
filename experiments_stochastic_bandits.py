29da58140d352281f80fe04da2af6ae27047ed3b
JesperDannath <39453670+JesperDannath@users.noreply.github.com>
2020-12-17 18:27:24 +0100

    Refactoring and Experiments
    -Refactoring of Exports (Now in Experiments for Flexibility)
    -Refactoring of Logging (Forecaster-specific logging enabled)
    -Experiments with two-armed Bandits

--- experiments_stochastic_bandits.py
+++ experiments_stochastic_bandits.py
@@ -10,16 +10,29 @@ from stochastic_bandit import simulation_environment as env
 from stochastic_bandit import ucb_forecaster
 from stochastic_bandit import bernoulli
 from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
 
 
 #Hilfsfunktionen
-def plot_list(l):
+def plot_list(l, title="", xlabel="", ylabel="", labels="",
+              axis_data=[]):
+    if len(axis_data==0):
+        axis_data=list(range(len(l[0])))
     for i in range(0,len(l)):
+        #plt.plot(np.stack((l[i], axis_data), axis=1))
         plt.plot(l[i])
+    plt.title(title, fontdict={"fontweight": "bold"})
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.legend(labels=labels)
+    plt.xticks(np.arange(0,len(l[0]), step=int(len(l[0]))/len(axis_data)),
+               labels=axis_data)
+
 
 #Experiments
 #number of reps
-reps = 10
+reps = 5
 
 ###Two arm bandit
 
@@ -30,11 +43,33 @@ env1 = env(bandit, ucb_forecaster())
 #env = env(bandit, random_forecaster)
 
 env1.play_round(rounds=12)
-res = env1.export_results("results_stochastic_bandit/Bernoulli_Experiment_12.csv")
+
+def export_result_sb(env, file):
+    dictionary = {"Player rewards": env.forecaster_rewards,
+                     "Player actions": env.actions}
+    experiment_data = pd.DataFrame(data=dictionary)
+    true_rewards = pd.DataFrame(env.true_rewards,
+                                columns = [
+                                "True Reward Arm "+str(i) for i in range(0,env.bandit.K)])
+    mu_hat = pd.DataFrame(env.mu_hat,
+                          columns = [
+                          "Mu Hat Arm "+str(i) for i in range(0,env.bandit.K)])
+    psi_s_inverse = pd.DataFrame(env.psi_s_inverse,
+                                 columns = [
+                                 "P.S.I "+str(i) for i in range(0,env.bandit.K)])
+    experiment_data = pd.concat([experiment_data,
+                                 true_rewards,
+                                 mu_hat,
+                                 psi_s_inverse], axis=1)
+    if file != None:
+        experiment_data.to_csv(file, index=False)
+    return(experiment_data)
+
+res = export_result_sb(env1, file="results_stochastic_bandit/Bernoulli_Experiment_12.csv")
 
 
 #Many runs development of regret and pseudo_regret
-def increase_T(max_T, env):
+def increase_T(max_T, env, reps=reps):
     regret = np.zeros(max_T)
     pseudo_regret = np.zeros(max_T)
     for T in range(1, max_T+1):
@@ -45,12 +80,68 @@ def increase_T(max_T, env):
     
 regret, pseudo_regret = increase_T(100, env1)
         
-plot_list(increase_T(100, env1))
+plot_list(increase_T(100, env1), 
+          title="(Pseudo-) Regret per Timestep for increasing \n Number of Rounds",
+          ylabel="Values",
+          xlabel="Timesteps",
+          labels=["Regret", "Pseudo-Regret"],
+          axis_data = np.arange(0, 100, step=10))
+#40 runs seem to be sufficient for convergence
 
 
+#Many runs effect of alpha
+def increase_alpha(min_alpha, max_alpha,
+                   step_size, env, T=40, reps=reps):
+    steps = int((max_alpha-min_alpha)/step_size)
+    alpha = min_alpha
+    log_alpha = np.zeros(steps)
+    regret = np.zeros(steps)
+    pseudo_regret = np.zeros(steps)
+    for step in range(0, steps):
+        env.forecaster.set_alpha(alpha)
+        env.play_many_rounds(T, reps, True)
+        pseudo_regret[step]=env.mean_pseudo_regret
+        regret[step]=env.get_regret()
+        log_alpha[step]=alpha
+        alpha += step_size
+    return(regret, pseudo_regret, log_alpha)
+    
+alpha_incr_2class = increase_alpha(min_alpha=0.1, max_alpha=4,
+                                   step_size=0.1, env=env1)
 
+plot_list(alpha_incr_2class[0:2],
+          title="Effect of Increasing alpha",
+          labels=["Regret", "Pseudo-Regret"],
+          axis_data=np.arange(np.min(alpha_incr_2class[2]),
+                              np.max(alpha_incr_2class[2]),
+                              step=0.5))
+#0.5 seems to be a good alpha value
 
-
-
-
+#Many runs effect of p-gap
+def increase_p_gap(step_size, env, T=40, reps=reps):
+    steps = int(1/step_size)
+    regret = np.zeros(steps)
+    pseudo_regret = np.zeros(steps)
+    gap = np.zeros(steps)
+    p_suboptimal=0.5
+    p_optimal = 0.5
+    for step in range(0, steps):
+        env.bandit.set_arms(
+                arms=[bernoulli(p_suboptimal),
+                       bernoulli(p_optimal)],
+                       expected_values=[p_suboptimal, p_optimal])
+        env.play_many_rounds(T, reps, True)
+        pseudo_regret[step]=env.mean_pseudo_regret
+        regret[step]=env.get_regret()
+        p_suboptimal -= step_size/2
+        p_optimal += step_size/2
+        gap[step] = p_optimal-p_suboptimal
+    return(regret, pseudo_regret, gap)
+    
+incr_pgap_2class = increase_p_gap(0.01, env1)
+    
+plot_list(incr_pgap_2class[0:2],
+          title="Effect of increasing p-gap",
+          labels=["Regret", "Pseudo-Regret"],
+          axis_data=np.arange(0.01, 1, step=0.5))
 
--- stochastic_bandit.py
+++ stochastic_bandit.py
@@ -8,7 +8,6 @@ Created on Sun Nov 29 12:29:42 2020
 import numpy as np
 import random
 from scipy.stats import bernoulli as ber
-import pandas as pd
 
 #Firstly, the Bandit and Forecaster Environment is created
 class simulation_environment():
@@ -27,16 +26,17 @@ class simulation_environment():
         self.true_rewards = np.zeros((rounds, self.bandit.K))
             
     def play_round(self, rounds, factor=1):
-        if factor==1: self.reset_values(rounds)
+        if factor==1: 
+            self.reset_values(rounds)
+            self.forecaster.reset_rounds(rounds, self.bandit.K, 1)
         true_rewards = self.bandit.generate_reward_table(rounds)
         forecaster_rewards = np.zeros(rounds)
         self.actions = np.zeros(rounds)
-        mu_hat = np.zeros((rounds, self.bandit.K))
-        psi_s_inverse = np.zeros((rounds, self.bandit.K))
         #Iterate through time
         for t in range(0, rounds):
             #Forecaster
-            action, mu_hat[t], psi_s_inverse[t] = self.forecaster.predict(
+            #!!! Median-Action?
+            action = self.forecaster.predict(
                     K=self.bandit.K,
                     rewards=forecaster_rewards[0:t],
                     actions=self.actions[0:t])
@@ -44,12 +44,11 @@ class simulation_environment():
             forecaster_rewards[t]+=true_rewards[t,action]
         self.true_rewards += np.multiply(true_rewards, factor)
         self.forecaster_rewards += np.multiply(forecaster_rewards, factor)
-        self.mu_hat += np.multiply(mu_hat, factor)
-        self.psi_s_inverse += np.multiply(psi_s_inverse, factor)
             #log calculations
             
     def play_many_rounds(self, rounds, repetitions, log_pseudo_regret=False):
         self.reset_values(rounds)
+        self.forecaster.reset_rounds(rounds, self.bandit.K, repetitions)
         pseudo_regret = 0
         for i in range(0, repetitions):
             self.play_round(rounds, factor=1/repetitions)
@@ -72,26 +71,6 @@ class simulation_environment():
         for action in self.actions:
             pseudo_regret -= expected_values[action.astype(int)]
         return(pseudo_regret)
-            
-#!!! Zu den Experimenten packen, muss individualisierbar sein!        
-    def export_results(self, file):
-        dictionary = {"Player rewards": self.forecaster_rewards,
-                      "Player actions": self.actions}
-        experiment_data = pd.DataFrame(data=dictionary)
-        true_rewards = pd.DataFrame(self.true_rewards,
-                                    columns = ["True Reward Arm "+str(i) for i in range(0,self.bandit.K)])
-        mu_hat = pd.DataFrame(self.mu_hat,
-                              columns = ["Mu Hat Arm "+str(i) for i in range(0,self.bandit.K)])
-        psi_s_inverse = pd.DataFrame(self.psi_s_inverse,
-                                     columns = ["P.S.I "+str(i) for i in range(0,self.bandit.K)])
-        experiment_data = pd.concat([experiment_data,
-                                     true_rewards,
-                                     mu_hat,
-                                     psi_s_inverse],
-                axis=1)
-        if file != None:
-            experiment_data.to_csv(file, index=False)
-        return(experiment_data)
     
 
 #Now, we define the stochastic bandit
@@ -106,6 +85,10 @@ class stochastic_bandit():
         self.t = 1
         self.shedule=shedule
         self.expected_values = expected_values
+        
+    def set_arms(self, arms, expected_values=None):
+        self.arms=arms
+        self.expected_values=expected_values
     
     #Draws a value from the Distribution or shedule of an arm    
     def draw(self, arm, time=None):
@@ -152,6 +135,17 @@ class ucb_forecaster():
     def __init__(self, psi_star_inverse=lambda x: np.sqrt(x/2), alpha=1):
         self.psi_star_inverse = psi_star_inverse
         self.alpha = alpha
+        self.rounds=1
+        
+    def set_alpha(self, alpha):
+        self.alpha=alpha
+        
+    def reset_rounds(self, rounds, K, reps):
+        self.rounds=rounds
+        self.reps=reps
+        self.K = K
+        self.mu_hat_list = np.zeros((self.rounds, self.K))
+        self.p_s_i_list = np.zeros((self.rounds, self.K))
     
     def predict(self, K, rewards, actions):
         t = len(rewards)+1
@@ -177,7 +171,9 @@ class ucb_forecaster():
             maximum = maximum[0]
         else:
             maximum = maximum[random.randint(0,len(maximum)-1)]
-        return(maximum, mu_hat_list, p_s_i_list)
+        self.mu_hat_list[t-1]+=np.multiply(mu_hat_list, (1/self.reps))
+        self.p_s_i_list[t-1]+=np.multiply(p_s_i_list, (1/self.reps))
+        return(maximum)
         
     
 
